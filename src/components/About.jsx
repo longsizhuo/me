0b23e5271ee567162adb71e9b6c0b1c6f16801cd
Loong Loong <longsizhuo@gmail.com>
2025-08-20 01:48:35 +0800

    fix: fix GITHUB TOKEN

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,71 +1,69 @@
-import React, {useEffect, useState} from "react";
-import { Tilt }from "react-tilt";
 import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { Tilt } from "react-tilt";
 
-import { styles } from "../styles";
-import { SectionWrapper } from "../hoc";
-import { fadeIn, textVariant } from "../utils/motion.ts";
-import {graphql} from "@octokit/graphql";
 import Typography from "@mui/material/Typography";
+import { graphql } from "@octokit/graphql";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion.ts";
 
-
-const GITHUB_TOKEN = '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';
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 const ServiceCard = ({ index, title, icon, description, stars, forks }) => (
-    <Tilt className="xs:w-[250px] w-full">
-        <motion.div
-            whileInView={{ opacity: 1, y: 0 }} // 当视图中时播放动画
-            initial={{ opacity: 0, y: 50 }}    // 初始状态
-            transition={{ duration: 0.5 }}     // 动画时长
-            variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-            className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      whileInView={{ opacity: 1, y: 0 }} // 当视图中时播放动画
+      initial={{ opacity: 0, y: 50 }} // 初始状态
+      transition={{ duration: 0.5 }} // 动画时长
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[320px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt={title}
+          className="w-20 h-20 object-contain" // 调整图片大小
+        />
+
+        <Typography
+          variant="h6"
+          component="h3"
+          className="text-white font-bold text-center"
+          noWrap
+          style={{ width: "100%" }} // 确保宽度限制，才会截断
         >
-            <div
-                options={{
-                    max: 45,
-                    scale: 1,
-                    speed: 450,
-                }}
-                className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[320px] flex justify-evenly items-center flex-col"
-            >
-                <img
-                    src={icon}
-                    alt={title}
-                    className="w-20 h-20 object-contain" // 调整图片大小
-                />
-
-
-                <Typography
-                    variant="h6"
-                    component="h3"
-                    className="text-white font-bold text-center"
-                    noWrap
-                    style={{width: "100%"}} // 确保宽度限制，才会截断
-                >
-                    {title}
-                </Typography>
-                <p className="text-white text-[14px] text-center">{description}</p>
-                <p className="text-white text-[14px] text-center">
-                    ⭐ Stars: {stars} | 🍴 Forks: {forks}
-                </p>
-            </div>
-        </motion.div>
-    </Tilt>
+          {title}
+        </Typography>
+        <p className="text-white text-[14px] text-center">{description}</p>
+        <p className="text-white text-[14px] text-center">
+          ⭐ Stars: {stars} | 🍴 Forks: {forks}
+        </p>
+      </div>
+    </motion.div>
+  </Tilt>
 );
 
 const About = () => {
-    const [services, setServices] = useState([]); // State to hold dynamic GitHub data
+  const [services, setServices] = useState([]); // State to hold dynamic GitHub data
 
-    useEffect(() => {
-        const fetchPinnedRepos = async () => {
-            const graphqlWithAuth = graphql.defaults({
-                headers: {
-                    authorization: `token ${GITHUB_TOKEN}`,
-                },
-            });
+  useEffect(() => {
+    const fetchPinnedRepos = async () => {
+      const graphqlWithAuth = graphql.defaults({
+        headers: {
+          authorization: `token ${GITHUB_TOKEN}`,
+        },
+      });
 
-            try {
-                const {user} = await graphqlWithAuth(`
+      try {
+        const { user } = await graphqlWithAuth(`
           {
             user(login: "longsizhuo") {
               pinnedItems(first: 4, types: REPOSITORY) {
@@ -87,46 +85,45 @@ const About = () => {
           }
         `);
 
-                const dynamicServices = user.pinnedItems.nodes.map((repo, index) => ({
-                    title: repo.name,
-                    icon: repo.owner.avatarUrl,
-                    description: repo.description,
-                    stars: repo.stargazerCount,
-                    forks: repo.forkCount,
-                    index,
-                }));
+        const dynamicServices = user.pinnedItems.nodes.map((repo, index) => ({
+          title: repo.name,
+          icon: repo.owner.avatarUrl,
+          description: repo.description,
+          stars: repo.stargazerCount,
+          forks: repo.forkCount,
+          index,
+        }));
 
-                setServices(dynamicServices); // Update services with GitHub data
+        setServices(dynamicServices); // Update services with GitHub data
+      } catch (error) {
+        console.error("Error fetching pinned repos:", error);
+      }
+    };
 
-            } catch (error) {
-                console.error('Error fetching pinned repos:', error);
-            }
-        };
+    fetchPinnedRepos();
+  }, []);
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Contribute.</h2>
+      </motion.div>
 
-        fetchPinnedRepos();
-    }, []);
-    return (
-        <>
-            <motion.div variants={textVariant()}>
-                <p className={styles.sectionSubText}>Introduction</p>
-                <h2 className={styles.sectionHeadText}>Contribute.</h2>
-            </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Below are some of the GitHub repositories I have contributed to,
+        including projects that I have pinned on my GitHub profile.
+      </motion.p>
 
-            <motion.p
-                variants={fadeIn("", "", 0.1, 1)}
-                className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-            >
-                Below are some of the GitHub repositories I have contributed to, including projects that I have pinned on my GitHub profile.
-
-            </motion.p>
-
-            <div className='mt-20 flex flex-wrap gap-10'>
-                {services.map((service, index) => (
-                    <ServiceCard key={service.title} index={index} {...service} />
-                ))}
-            </div>
-        </>
-    );
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 const AboutWithWrapper = SectionWrapper(About, "about");
--- src/components/GitHubCard.jsx
+++ src/components/GitHubCard.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { graphql } from '@octokit/graphql';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Avatar from '@mui/material/Avatar';
-import Grid from '@mui/material/Grid';
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { graphql } from "@octokit/graphql";
+import React, { useEffect, useState } from "react";
 
-const GITHUB_TOKEN = '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'; // 请将此替换为你的GitHub Token
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 const GitHubCard = () => {
-    const [pinnedRepos, setPinnedRepos] = useState([]);
+  const [pinnedRepos, setPinnedRepos] = useState([]);
 
-    useEffect(() => {
-        const fetchPinnedRepos = async () => {
-            const graphqlWithAuth = graphql.defaults({
-                headers: {
-                    authorization: `token ${GITHUB_TOKEN}`,
-                },
-            });
+  useEffect(() => {
+    const fetchPinnedRepos = async () => {
+      const graphqlWithAuth = graphql.defaults({
+        headers: {
+          authorization: `token ${GITHUB_TOKEN}`,
+        },
+      });
 
-            try {
-                const { user } = await graphqlWithAuth(`
+      try {
+        const { user } = await graphqlWithAuth(`
                     {
                         user(login: "longsizhuo") {
                             pinnedItems(first: 3, types: REPOSITORY) {
@@ -43,48 +43,53 @@ const GitHubCard = () => {
                         }
                     }
                 `);
-                setPinnedRepos(user.pinnedItems.nodes);
-            } catch (error) {
-                console.error('Error fetching pinned repos:', error);
-            }
-        };
+        setPinnedRepos(user.pinnedItems.nodes);
+      } catch (error) {
+        console.error("Error fetching pinned repos:", error);
+      }
+    };
 
-        fetchPinnedRepos().then(r =>
-            console.log(r)
-        );
-    }, []);
+    fetchPinnedRepos().then((r) => console.log(r));
+  }, []);
 
-    return (
-        <Box>
-            <h2 style={{color: '#333'}}>First 3 Contributed</h2>
-            <Grid container spacing={2}>
-            {pinnedRepos.map((repo) => (
-                    <Grid item xs={12} sm={6} md={4} key={repo.id}>
-                        <Card sx={{ maxWidth: "auto", minHeight: 250 }}> {/* 调整卡片大小 */}
-                            <CardMedia>
-                                <Avatar
-                                    alt={repo.owner.login}
-                                    src={repo.owner.avatarUrl}
-                                    sx={{ width: 48, height: 48, margin: 'auto', marginTop: '10px' }}
-                                />
-                            </CardMedia>
-                            <CardContent>
-                                <Typography variant="subtitle1" component="div">
-                                    {repo.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {repo.description}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Stars: {repo.stargazerCount} | Forks: {repo.forkCount}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box>
+      <h2 style={{ color: "#333" }}>First 3 Contributed</h2>
+      <Grid container spacing={2}>
+        {pinnedRepos.map((repo) => (
+          <Grid item xs={12} sm={6} md={4} key={repo.id}>
+            <Card sx={{ maxWidth: "auto", minHeight: 250 }}>
+              {" "}
+              {/* 调整卡片大小 */}
+              <CardMedia>
+                <Avatar
+                  alt={repo.owner.login}
+                  src={repo.owner.avatarUrl}
+                  sx={{
+                    width: 48,
+                    height: 48,
+                    margin: "auto",
+                    marginTop: "10px",
+                  }}
+                />
+              </CardMedia>
+              <CardContent>
+                <Typography variant="subtitle1" component="div">
+                  {repo.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {repo.description}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Stars: {repo.stargazerCount} | Forks: {repo.forkCount}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default GitHubCard;
