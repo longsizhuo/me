f8d5ba9d8be5ee1c74100c4666d6205f460f9d3d
Loong Loong <longsizhuo@gmail.com>
2025-08-31 17:03:33 +0800

    chore: update ESLint configuration and pnpm lockfile with new dependencies

--- src/hoc/TYPE.ts
+++ src/hoc/TYPE.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+// 这个文件包含类型定义，未使用的变量是预期的
 import { ComponentType, FC } from 'react';
 
 export type SectionWrapperType = <P>(_Component: ComponentType<P>, _idName: string) => FC;
